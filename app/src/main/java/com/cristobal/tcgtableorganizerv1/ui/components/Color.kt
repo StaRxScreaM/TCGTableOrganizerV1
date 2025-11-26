21aad8fd5748f2116434878c3898aa0c1f95421a
StaRxScreaM <cristobal.ta.co@gmail.com>
2025-11-26 03:42:12 -0300

    Update en Mesas, punto para restaurar

--- app/src/main/java/com/cristobal/tcgtableorganizerv1/ui/components/Color.kt
+++ app/src/main/java/com/cristobal/tcgtableorganizerv1/ui/components/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 // Colores principales de la app
 val Burgundy = Color(0xFF6A0F19)
 val SurfaceGray = Color(0xFFE8E8E8)
-val SecondaryGray = Color(0xFF888888)
-val White = Color(0xFFFFFFFF)
+val SecondaryGray = Color(0xFFFFFFFF)
+val White = Color(0xFF6C6161)
