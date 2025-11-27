1a6f072ddedee2ab0b26cda0e8dba732e7b196ae
StaRxScreaM <cristobal.ta.co@gmail.com>
2025-11-26 23:58:23 -0300

    Merge remote-tracking branch 'origin/master'

--- app/src/androidTest/java/com/cristobal/tcgtableorganizerv1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cristobal/tcgtableorganizerv1/ExampleInstrumentedTest.kt
@@ -1,24 +1,29 @@
 package com.cristobal.tcgtableorganizerv1
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Instrumented test, se ejecuta en un dispositivo/emulador Android.
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun useAppContext_packageNameIsCorrect() {
+        // Contexto de la app en el dispositivo de prueba.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.cristobal.tcgtableorganizerv1", appContext.packageName)
+    }
+
+    @Test
+    fun appNameString_isNotEmpty() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appName = appContext.getString(R.string.app_name)
+
+        assertTrue("El nombre de la app no debería estar vacío", appName.isNotBlank())
     }
 }
--- app/src/test/java/com/cristobal/tcgtableorganizerv1/ExampleUnitTest.kt
+++ app/src/test/java/com/cristobal/tcgtableorganizerv1/ExampleUnitTest.kt
@@ -1,17 +1,51 @@
 package com.cristobal.tcgtableorganizerv1
 
+import com.cristobal.tcgtableorganizerv1.model.EventUi
+import com.cristobal.tcgtableorganizerv1.model.TableStatus
+import com.cristobal.tcgtableorganizerv1.model.TableUi
+import org.junit.Assert.*
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun eventUi_holdsCorrectData() {
+        val event = EventUi(
+            dayShort = "ENE",
+            dayNumber = "10",
+            time = "10:00",
+            title = "Commander Casual",
+            storeLabelTime = "Tienda X - 10:00"
+        )
+
+        assertEquals("ENE", event.dayShort)
+        assertEquals("10", event.dayNumber)
+        assertEquals("10:00", event.time)
+        assertEquals("Commander Casual", event.title)
+        assertEquals("Tienda X - 10:00", event.storeLabelTime)
+    }
+
+    @Test
+    fun tableUi_createsValidTable() {
+        val table = TableUi(
+            id = 1,
+            label = "MESA 1",
+            game = "MTG Commander",
+            status = TableStatus.SEARCHING,
+            playerSlots = 4,
+            reserved = true
+        )
+
+        assertEquals(1, table.id)
+        assertEquals("MESA 1", table.label)
+        assertEquals("MTG Commander", table.game)
+        assertEquals(TableStatus.SEARCHING, table.status)
+        assertEquals(4, table.playerSlots)
+        assertTrue(table.reserved)
     }
 }
